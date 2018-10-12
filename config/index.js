f10c428810f8599aa3984977f38b29e1841807f6
gd <1356874539@qq.com>
2018-10-12 10:20:13 +0800

    ip地址修改

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     devTest: { //部署到测试服务器上
         remotePath: '/app/', //部署到服务器的路径
-        host: '111.11.111.111', //ip地址
+        host: '111.11.111.111', //ip地址 ip地址修改
         user: 'root', //帐号
         pass: '88888888', //密码
         port: 22 //端口
